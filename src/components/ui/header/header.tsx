da9346b93c53532e7268115f0a1f5a241de71347
mathias akari <cyphercode59@gmail.com>
2023-12-05 20:52:05 +0100

    new

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router";
 import "./header.styles.css";
 
 export const Header = ()=>{
+    const navigate = useNavigate();
+
+    const link = ()=>{
+        navigate("/")
+    }
     return(
         <header className="header-wrap">
              <div className="app-logo">
-                <h1>ElQueue Logistic System</h1>
+                <h1 onClick={link}>ElQueue Logistic System</h1>
              </div>
         </header>
     )
